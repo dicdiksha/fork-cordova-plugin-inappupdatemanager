c60ed52e8d647a95a2fbe3a492fb430f060f5b5d
Sayali More <sayalimore@admins-MacBook-Pro.local>
2024-09-26 15:45:25 +0530

    updated-play-core-lib-1.10.3

--- src/android/InAppUpdateManager.java
+++ src/android/InAppUpdateManager.java
@@ -8,7 +8,7 @@ import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.install.model.AppUpdateType;
 import com.google.android.play.core.install.model.UpdateAvailability;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.Task;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
